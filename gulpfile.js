0dbf5d025c4bdef24e51a4371d383cb87a1aa553
Lionel Breduillieard <lionel@lumographe.fr>
2014-05-28 17:35:46 +0200

    Add Gulp task runner

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+/* jshint node:true */
+'use strict';
+
+var gulp = require('gulp');
+var react = require('gulp-react');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
+
+var opt = {
+  outputFolder:'build',
+
+  server:{
+    port:4000,
+    livereload: 31357
+  },
+
+  cssAssets: [
+    'bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'css/kept.css'
+  ],
+
+  jsAssets: [
+    'bower_components/react/react.min.js',
+    'bower_components/react-bootstrap/react-bootstrap.min.js'
+  ],
+
+  htmlAssets: [
+    'index.html'
+  ],
+
+  mainJs:{
+    src:'src/kept.jsx',
+    dist: 'dist/js/kept.js'
+  }
+
+};
+
+
+
+/**
+ * Assets tasks
+ */
+gulp.task('assets',['assets:html', 'assets:js', 'assets:css']);
+
+gulp.task('assets:html', function(){
+  return gulp.src(opt.htmlAssets)
+    .pipe(gulp.dest(opt.outputFolder));
+});
+
+gulp.task('assets:js', function(){
+  return gulp.src(opt.jsAssets)
+    .pipe(gulp.dest(opt.outputFolder+'/js/lib'));
+});
+
+gulp.task('assets:css', function(){
+  return gulp.src(opt.cssAssets)
+    .pipe(gulp.dest(opt.outputFolder+'/css'));
+});
+
+
+gulp.task('transpile' , function(){
+  return gulp.src(opt.mainJs.src)
+    .pipe(plumber())
+    .pipe(react())
+    .pipe(gulp.dest(opt.outputFolder+'/js'));
+});
+
+gulp.task('uglify' ,['transpile'], function(){
+  return gulp.src(opt.mainJs.dist)
+    .pipe(uglify() )
+    .pipe(gulp.dest(opt.outputFolder+'/js'));
+});
+
+/**
+ * Server task
+ */
+gulp.task('server', function() {
+  connect.server({
+    root: opt.outputFolder,
+    port: opt.server.port,
+    livereload: {
+      port:opt.livereload
+    }
+  });
+});
+
+/**
+ * Watch task
+ * Launch a server with livereload
+ */
+gulp.task('watch', ['dev'], function(){
+  gulp.watch(opt.cssAssets,['assets:css']);
+  gulp.watch(opt.jsAssets,['assets:js']);
+  gulp.watch(opt.htmlAssets,['assets:html']);
+  gulp.watch(opt.mainJs.src,['transpile']);
+
+  gulp.watch([opt.outputFolder + '/**/*.*'])
+    .on('change', function(){
+      // trigger the live reload server
+      gulp.src('').pipe(connect.reload());
+    });
+});
+
+gulp.task('dev', ['assets', 'transpile']);
+gulp.task('dist', ['assets', 'transpile', 'uglify']);
+gulp.task('default', ['server', 'watch']);
