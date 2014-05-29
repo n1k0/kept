eb256d2e47ae83a7616a547cf7d815c030d67803
Nicolas Perriault <nperriault@gmail.com>
2014-05-29 19:19:40 +0200

    Update 2014-05-29T17:19:40.966Z

--- js/kept.js
+++ js/kept.js
@@ -3,23 +3,23 @@
 var initial = [
   {id: 1, type: "text", title: "This is a Text entry", text: "Hello World."},
   {id: 2, type: "text", title: "Lorem ipsum dolor sit amet",
-   text: "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+   text: "**Consectetur adipisicing elit**, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
   {id: 3, type: "todo", title: "Today", tasks: [
     {label: "Walk the dog", done: true},
     {label: "Clean the car", done: false},
     {label: "Buy a new carpet", done: false}
   ]},
   {id: 4, type: "text", title: "Ut enim ad minim veniam",
-   text: "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+   text: "Quis nostrud *exercitation ullamco* laboris nisi ut aliquip ex ea commodo consequat."},
   {id: 5, type: "text", title: "Duis aute irure",
-   text: "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
+   text: "> Dolor in [reprehenderit](http://google.com/) in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
   {id: 6, type: "text", title: "Excepteur sint occaecat",
-   text: "Cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+   text: "    Cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
   {id: 7, type: "todo", title: "Trip to Paris", tasks: [
-    {label: "Buy cheese", done: false},
-    {label: "Buy more cheese", done: false},
+    {label: "Eat cheese", done: false},
+    {label: "Eat more cheese", done: false},
     {label: "See Eiffel Tower", done: false},
-    {label: "Buy even more cheese", done: false}
+    {label: "MOAR CHEESE", done: false}
   ]}
 ];
 
@@ -54,10 +54,15 @@ function KeptStore(options) {
 
 KeptStore.prototype = {
   load: function() {
+    var data = this._store.getItem("keep.data");
+    if (!data) {
+      console.error("empty stored kept data:", data);
+      return [];
+    }
     try {
-      return JSON.parse(this._store.getItem("keep.data")) || [];
+      return JSON.parse(data) || [];
     } catch (e) {
-      console.error("failed loading keep data", e);
+      console.error("failed parsing kept data:", data, e);
       return [];
     }
   },
@@ -77,7 +82,60 @@ var GlyphiconLink = React.createClass({displayName: 'GlyphiconLink',
   }
 });
 
+// http://prometheusresearch.github.io/react-forms/examples/undo.html
+var UndoStack = {
+  getInitialState: function() {
+    return {undo: [], redo: []};
+  },
+
+  snapshot: function() {
+    var undo = this.state.undo.concat(this.getStateSnapshot());
+    this.setState({undo: undo, redo: []});
+  },
+
+  hasUndo: function() {
+    return this.state.undo.length > 0;
+  },
+
+  hasRedo: function() {
+    return this.state.redo.length > 0;
+  },
+
+  redo: function() {
+    this._undoImpl(true);
+  },
+
+  undo: function() {
+    this._undoImpl();
+  },
+
+  _undoImpl: function(isRedo) {
+    var undo = this.state.undo.slice(0);
+    var redo = this.state.redo.slice(0);
+    var snapshot;
+
+    if (isRedo) {
+      if (redo.length === 0) {
+        return;
+      }
+      snapshot = redo.pop();
+      undo.push(this.getStateSnapshot());
+    } else {
+      if (undo.length === 0) {
+        return;
+      }
+      snapshot = undo.pop();
+      redo.push(this.getStateSnapshot());
+    }
+
+    this.setStateSnapshot(snapshot);
+    this.setState({undo:undo, redo:redo});
+  }
+};
+
 var KeptApp = React.createClass({displayName: 'KeptApp',
+  mixins: [UndoStack],
+
   store: new KeptStore(),
 
   getInitialState: function() {
@@ -86,7 +144,16 @@ var KeptApp = React.createClass({displayName: 'KeptApp',
     };
   },
 
+  getStateSnapshot: function() {
+    return {items: this.state.items};
+  },
+
+  setStateSnapshot: function(snapshot) {
+    this.setState(snapshot);
+  },
+
   _forms: {
+    // XXX: use standard component constructor?
     text: function(data) {
       return KeptTextForm( {resetForm:this.resetForm, create:this.create, update:this.update, data:data} );
     },
@@ -96,9 +163,14 @@ var KeptApp = React.createClass({displayName: 'KeptApp',
     }
   },
 
+  save: function(items) {
+    this.store.save(items);
+    this.snapshot();
+    this.setState({items: items});
+  },
+
   loadSamples: function() {
-    this.store.save(initial);
-    this.setState({items: initial});
+    this.save(initial);
   },
 
   formCreator: function(type) {
@@ -117,9 +189,8 @@ var KeptApp = React.createClass({displayName: 'KeptApp',
 
   create: function(itemData) {
     itemData.id = nextId(this.state.items);
-    var items = this.state.items.concat([itemData]);
-    this.setState({form: null, items: items});
-    this.store.save(items);
+    this.save(this.state.items.concat([itemData]));
+    this.resetForm();
   },
 
   edit: function(itemData) {
@@ -127,35 +198,33 @@ var KeptApp = React.createClass({displayName: 'KeptApp',
   },
 
   update: function(updatedItem) {
-    var items = this.state.items.map(function(item) {
+    this.save(this.state.items.map(function(item) {
       if (item.id === updatedItem.id)
         return updatedItem;
       return item;
-    });
-    this.store.save(items);
-    this.setState({form: null, items: items});
+    }));
+    this.resetForm();
   },
 
   remove: function(itemData) {
-    var items = this.state.items.filter(function(data) {
+    this.save(this.state.items.filter(function(data) {
       return itemData !== data;
-    });
-    this.setState({items: items});
-    this.store.save(items);
+    }));
   },
 
   move: function(fromIndex, toIndex) {
-    var items = this.state.items;
+    var items = this.state.items.slice(0);
     items.splice(toIndex, 0, items.splice(fromIndex, 1)[0]);
-    this.setState({items: items});
-    this.store.save(items);
+    this.save(items);
   },
 
   render: function() {
     return (
       React.DOM.div(null, 
         KeptMenuBar( {newItem:this.newItem,
-                     loadSamples:this.loadSamples} ),
+                     loadSamples:this.loadSamples,
+                     undo:this.undo,
+                     redo:this.redo} ),
         this.state.form,
         KeptItems( {items:this.state.items,
                    newItem:this.newItem,
@@ -182,6 +251,8 @@ var KeptMenuBar = React.createClass({displayName: 'KeptMenuBar',
             React.DOM.ul( {className:"nav navbar-nav"}, 
               React.DOM.li(null, React.DOM.a( {href:"#", onClick:this.props.newItem("text")}, "Text")),
               React.DOM.li(null, React.DOM.a( {href:"#", onClick:this.props.newItem("todo")}, "Todo")),
+              React.DOM.li(null, React.DOM.a( {href:"#", onClick:this.props.undo}, "Undo")),
+              React.DOM.li(null, React.DOM.a( {href:"#", onClick:this.props.redo}, "Redo")),
               React.DOM.li(null, React.DOM.a( {href:"#", onClick:this.props.loadSamples}, "Load samples"))
             )
           )
@@ -396,13 +467,13 @@ var KeptTodo = React.createClass({displayName: 'KeptTodo',
   },
 
   updateTasks: function(tasks) {
+    this.setState({tasks: tasks});
     this.props.update({
       type: "todo",
       id: this.props.data.id,
       title: this.props.data.title,
       tasks: tasks
     });
-    this.setState({tasks: tasks});
   },
 
   getProgress: function() {
