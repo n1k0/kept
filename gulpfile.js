29e938919a4a967db5e218e287f28d8a8dfcc54b
Nicolas Perriault <nperriault@gmail.com>
2014-05-29 12:35:43 +0200

    refs #12: Added gulp-gh-page based deploy task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,96 @@
 /* jshint node:true */
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var react = require('gulp-react');
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var connect = require('gulp-connect');
+var gulp = require("gulp");
+var react = require("gulp-react");
+var plumber = require("gulp-plumber");
+var uglify = require("gulp-uglify");
+var connect = require("gulp-connect");
+var deploy = require("gulp-gh-pages");
 
 var opt = {
-  outputFolder:'build',
+  outputFolder: "build",
 
   server:{
-    port:4000,
+    port: 4000,
     livereload: 31357
   },
 
-  fontAssets:[
-    'bower_components/bootstrap/fonts/*'
+  fontAssets: [
+    "bower_components/bootstrap/fonts/*"
   ],
 
   cssAssets: [
-    'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    'css/kept.css'
+    "bower_components/bootstrap/dist/css/bootstrap.min.css",
+    "bower_components/bootstrap/dist/css/bootstrap.min.css",
+    "css/kept.css"
   ],
 
   jsAssets: [
-    'bower_components/react/react.min.js',
-    'bower_components/react-bootstrap/react-bootstrap.min.js',
-    'bower_components/marked/lib/marked.js',
+    "bower_components/react/react.min.js",
+    "bower_components/react-bootstrap/react-bootstrap.min.js",
+    "bower_components/marked/lib/marked.js",
   ],
 
   htmlAssets: [
-    'index.html'
+    "index.html"
   ],
 
   mainJs:{
-    src:'src/kept.jsx',
-    dist: 'dist/js/kept.js'
+    src:  "src/kept.jsx",
+    dist: "dist/js/kept.js"
   }
 };
 
 /**
-* Assets tasks
-*/
-gulp.task('assets',['assets:html', 'assets:js', 'assets:fonts', 'assets:css']);
+ * Assets tasks
+ */
+gulp.task("assets", [
+  "assets:html",
+  "assets:js",
+  "assets:fonts",
+  "assets:css"
+]);
 
-gulp.task('assets:html', function(){
+gulp.task("assets:html", function() {
   return gulp.src(opt.htmlAssets)
     .pipe(gulp.dest(opt.outputFolder));
 });
 
-gulp.task('assets:fonts', function(){
+gulp.task("assets:fonts", function() {
   return gulp.src(opt.fontAssets)
-    .pipe(gulp.dest(opt.outputFolder+'/fonts'));
+    .pipe(gulp.dest(opt.outputFolder + "/fonts"));
 });
 
-gulp.task('assets:js', function(){
+gulp.task("assets:js", function() {
   return gulp.src(opt.jsAssets)
-    .pipe(gulp.dest(opt.outputFolder+'/js/lib'));
+    .pipe(gulp.dest(opt.outputFolder + "/js/lib"));
 });
 
-gulp.task('assets:css', function(){
+gulp.task("assets:css", function() {
   return gulp.src(opt.cssAssets)
-    .pipe(gulp.dest(opt.outputFolder+'/css'));
+    .pipe(gulp.dest(opt.outputFolder + "/css"));
 });
 
 
-gulp.task('transpile' , function(){
+gulp.task("transpile", function() {
   return gulp.src(opt.mainJs.src)
     .pipe(plumber())
     .pipe(react())
-    .pipe(gulp.dest(opt.outputFolder+'/js'));
+    .pipe(gulp.dest(opt.outputFolder + "/js"));
 });
 
-gulp.task('uglify' ,['transpile'], function(){
+gulp.task("uglify", ["transpile"], function() {
   return gulp.src(opt.mainJs.dist)
     .pipe(plumber())
     .pipe(uglify())
-    .pipe(gulp.dest(opt.outputFolder+'/js'));
+    .pipe(gulp.dest(opt.outputFolder + "/js"));
 });
 
 /**
-* Server task
-*/
-gulp.task('server', function() {
+ * Server task
+ */
+gulp.task("server", function() {
   connect.server({
     root: opt.outputFolder,
     port: opt.server.port,
@@ -95,21 +101,29 @@ gulp.task('server', function() {
 });
 
 /**
-* Watch task
-* Launch a server with livereload
-*/
-gulp.task('watch', ['assets', 'transpile'], function(){
-  gulp.watch(opt.cssAssets,['assets:css']);
-  gulp.watch(opt.jsAssets,['assets:js']);
-  gulp.watch(opt.htmlAssets,['assets:html']);
-  gulp.watch(opt.mainJs.src,['transpile']);
+ * Watch task
+ * Launch a server with livereload
+ */
+gulp.task("watch", ["assets", "transpile"], function() {
+  gulp.watch(opt.cssAssets, ["assets:css"]);
+  gulp.watch(opt.jsAssets, ["assets:js"]);
+  gulp.watch(opt.htmlAssets, ["assets:html"]);
+  gulp.watch(opt.mainJs.src, ["transpile"]);
 
-  gulp.watch([opt.outputFolder + '/**/*.*'])
-    .on('change', function(){
+  gulp.watch([opt.outputFolder + "/**/*.*"])
+    .on("change", function() {
       // trigger the live reload server
-      gulp.src('').pipe(connect.reload());
+      gulp.src("").pipe(connect.reload());
     });
 });
 
-gulp.task('dist', ['assets', 'transpile', 'uglify']);
-gulp.task('default', ['server', 'watch']);
+gulp.task("dist", ["assets", "transpile", "uglify"]);
+gulp.task("default", ["server", "watch"]);
+
+/**
+ * Deploy to gh-pages
+ */
+gulp.task("deploy", ["dist"], function() {
+  gulp.src("./build/**/*")
+      .pipe(deploy("git@github.com:n1k0/kept.git"));
+});
