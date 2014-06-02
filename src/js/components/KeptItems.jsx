23aa1809e643e277c2e6b7a6dd260221b72e13be
Nicolas Perriault <nperriault@gmail.com>
2014-06-02 11:59:30 +0200

    Fixes #16 - Added missing import for DefaultContent component.

--- src/js/components/KeptItems.jsx
+++ src/js/components/KeptItems.jsx
@@ -2,6 +2,7 @@
 
 var React = require("react");
 var KeptEntry = require("./KeptEntry");
+var DefaultContent = require("./DefaultContent");
 
 var KeepItems = React.createClass({
   render: function() {
